2d4f701c2af3a73d550ebdd162ee954fe2b7c8c4
Godwin Alex <69471820+Alekz19@users.noreply.github.com>
2020-08-25 12:27:40 +0100

    Update ncdc.js

--- ncdc.js
+++ ncdc.js
@@ -1,12 +1,13 @@
 "use strict";
-var dataTable = document.getElementById('database'),
-   submit = document.getElementById("data"),
-   update = document.getElementById("data-2"),
-   editBtn = "<input type='button'  class='edit-button' value='Edit' onclick='edit(this)'>";
+let rIndex;
+const dataTable = document.getElementById('database'),
+    submit = document.getElementById("data"),
+    update = document.getElementById("data-2");
+let rows = dataTable.children[1].getElementsByTagName('tr');
 //function that activate the checkbox status
-function checkBoxStatus(status) {
-var status = document.getElementById("checkBox").checked;
-    if (status) {
+let status = document.getElementById("checkBox");
+status.onchange = function checkBoxStatus() {
+    if (status.checked) {
      document.getElementById("checkBox").value = "Yes";
     } else {
     document.getElementById("checkBox").value = "No";
@@ -14,7 +15,7 @@ var status = document.getElementById("checkBox").checked;
 }
 //Page Date and Time Function
 function currentDate() {
-    var showDate = document.getElementById('showDate'),
+    let showDate = document.getElementById('showDate'),
         d = new Date(),
         days = ["Sun,", "Mon,", "Tue,", "Wed,", "Thu,", "Fri,", "Sat,"],
         today = days[d.getDay()],
@@ -48,13 +49,13 @@ function currentDate() {
         minute = "0" + minute;
     }
     showDate.textContent = today + " " + thisMonth + " " + date + " " + year + ". " + hour + ":" + minute + ":" + second + " " + meridian;
-    var t = setTimeout(function () {currentDate(); 
+    const t = setTimeout(function () {currentDate(); 
             }, 1000);
 }
 currentDate();
 //function that shows the submit form data in the table
 function showData() {
-    var row = document.createElement('tr'),
+    let tableRows = document.createElement('tr'),
         td1 = document.createElement('td'),
         td2 = document.createElement('td'),
         td3 = document.createElement('td'),
@@ -65,68 +66,53 @@ function showData() {
     td2.innerHTML = document.getElementById('email').value;
     td3.innerHTML = document.getElementById('gender').value;
     td4.innerHTML = document.getElementById("checkBox").value;
-    td5.innerHTML = editBtn;
+    td5.innerHTML = "<input type='button' class='edit-button' value='Edit' onclick='edit(this)'>";
     td6.innerHTML = "<input type='button' class='delete-button' value='Delete' onclick='delBtn(this)'>";
-    row.appendChild(td1);
-    row.appendChild(td2);
-    row.appendChild(td3);
-    row.appendChild(td4);
-    row.appendChild(td5);
-    row.appendChild(td6);
+    tableRows.appendChild(td1);
+    tableRows.appendChild(td2);
+    tableRows.appendChild(td3);
+    tableRows.appendChild(td4);
+    tableRows.appendChild(td5);
+    tableRows.appendChild(td6);
 submit.reset();
-    dataTable.children[1].insertBefore(row, dataTable.children[0].childNodes[row.length]);
+ dataTable.children[1].insertBefore(tableRows, dataTable.children[0].childNodes[tableRows.length]);
     return false;
 };
 //Function that edit any row being clicked
-function edit() {
-    var dataRows = dataTable.children[1].rows;
+function edit() { 
     submit.style.display = "none";
     update.style.display = "block";
-    for (var i = 0; i <= dataRows.length; i++) {
-     var currentRow = dataRows[i];
-currentRow.onclick = function editBtn() {
-            if (this.parentNode.nodeName == 'tbody') {
-                return false;
-            }
-            var cells = this.cells;
-            var name = cells[0].innerHTML;
-            var fullName = name.split(" ");
-            var fName = fullName[0];
-            var lName = fullName[1];
-            var f1 = document.getElementById('fname-2');
-            var f2 = document.getElementById('lname-2');
-            var f3 = document.getElementById('email-2');
-            var f4 = document.getElementById('gender-2');
-            var f5 = document.getElementById('checkBox2');
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].onclick = function editBtn() { 
+            rIndex = this.rowIndex;
+            let name = this.cells[0].innerHTML,
+            fullName = name.split(" "),
+            fName = fullName[0],
+            lName = fullName[1],
+            f1 = document.getElementById('fname-2'),
+            f2 = document.getElementById('lname-2'),
+            f3 = document.getElementById('email-2'),
+            f4 = document.getElementById('gender-2');
             f1.value = fName;
             f2.value = lName;
-            f3.value = cells[1].innerHTML;
-            f4.value = cells[2].innerHTML;
-            f5.value = cells[3].innerHTML;
-    };
-};
-}
-//function that update form data in the table
-    function updateData() {
-    update.style.display = "none";
-    submit.style.display = "block";
-var dataRows = dataTable.children[1].rows;
-    for (var i = 0; i <= dataRows.length; i++) {
-       var currentRow = dataRows[i];
-       var cells = currentRow.cells;
-       cells[0].innerHTML = document.getElementById('fname-2').value + " " + document.getElementById('lname-2').value;
-       cells[1].innerHTML = document.getElementById('email-2').value;
-       cells[2].innerHTML = document.getElementById('gender-2').value;
-       cells[3].innerHTML = document.getElementById('checkBox').value;
-       cells[4].innerHTML = editBtn;
-       cells[5].innerHTML = "<input type='button' class='delete-button' value='Delete' onclick='delBtn(this)'>";
+            f3.value = this.cells[1].innerHTML;
+            f4.value = this.cells[2].innerHTML;
+        };
+        //function that update form data in the table
+    update.onsubmit = function updateData() {
+        update.style.display = "none";
+        submit.style.display = "block";
+        rows[i].cells[0].innerHTML = document.getElementById('fname-2').value + " " + document.getElementById('lname-2').value;
+        rows[i].cells[1].innerHTML = document.getElementById('email-2').value;
+        rows[i].cells[2].innerHTML = document.getElementById('gender-2').value;
+        rows[i].cells[3].innerHTML = document.getElementById('checkBox').value;
+        rows[i].cells[4].innerHTML = "<input type='button'  class='edit-button' value='Edit' onclick='edit(this)'>";
+        rows[i].cells[5].innerHTML = "<input type='button' class='delete-button' value='Delete' onclick='delBtn(this)'>";
 return false;
     };
 };
+}
 //Function that delete any row being clicked
-        function delBtn(r) {
-            var j = r.parentNode.parentNode.rowIndex;       dataTable.deleteRow(j);
-}     
-
-
-
+    function delBtn(r) {
+        var del = r.parentNode.parentNode.rowIndex;       dataTable.deleteRow(del);
+};
